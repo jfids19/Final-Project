de3ae09f589bf5a7d300caa3ac932a61e8dc6cac
jfids19 <116359672+jfids19@users.noreply.github.com>
2024-03-07 13:17:25 +0000

    Cutscene and Final Movement Fix

---
+++ FinalProject/Assets/Scripts/Cutscene1.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Cutscene1 : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject cutscene;
+    public GameObject mainCamera;
+    public PlayableDirector timeline;
+
+    void Start()
+    {
+        cutscene.SetActive(false);
+        timeline.enabled = false;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        cutscene.SetActive(true);
+        mainCamera.SetActive(false);
+        player.SetActive(false);
+        timeline.enabled = true;
+        StartCoroutine(FinishCut());
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(9.17f);
+        mainCamera.SetActive(true);
+        player.SetActive(true);
+        cutscene.SetActive(false);
+    }
+}
